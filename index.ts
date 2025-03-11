ea161dcedaa924a3a351b1115daa4d62e8ce34c4
Diego <diegofelipe.14@gmail.com>
2025-03-11 13:21:12 -0400

    update

--- index.ts
+++ index.ts
@@ -1,17 +1,45 @@
 export class UniqueObjectsSet {
-  // Public so it can be accessed outside the class
   public set: Set<string>;
 
   constructor() {
     this.set = new Set();
   }
 
-  add(obj: object) {
+  add(obj: object): this {
     const key = JSON.stringify(obj);
     this.set.add(key);
+    return this;
   }
 
-  getAll() {
+  has(obj: object): boolean {
+    const key = JSON.stringify(obj);
+    return this.set.has(key);
+  }
+
+  delete(obj: object): boolean {
+    const key = JSON.stringify(obj);
+    return this.set.delete(key);
+  }
+
+  get size(): number {
+    return this.set.size;
+  }
+
+  getAll(): object[] {
     return Array.from(this.set).map((key) => JSON.parse(key));
+  }
+
+  [Symbol.iterator](): Iterator<object> {
+    const items = this.getAll();
+    let index = 0;
+    return {
+      next: (): IteratorResult<object> => {
+        if (index < items.length) {
+          return { value: items[index++], done: false };
+        } else {
+          return { value: undefined, done: true };
+        }
+      },
+    };
   }
 }
